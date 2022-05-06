9c5671359643f3b574b9fc419a1ff28632ab586c
YYH <1583963700@qq.com>
2022-05-06 14:02:28 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/gittset.java
+++ src/main/java/com/atguigu/git/gittset.java
@@ -4,5 +4,6 @@ public class gittset {
     public static void main(String[] args) {
         System.out.println("hello.git");
         System.out.println("hahah1");
+        System.out.println("这里hot-fix被修改了哦");
     }
 }
